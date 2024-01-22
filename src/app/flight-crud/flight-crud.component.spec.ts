9f38d529ef12f60219a9871e483475de9fd5d3a0
Atilaa1 <atila.kujundzic.5000@metropolitan.ac.rs>
2024-01-22 04:28:16 +0100

    IT255-DZ14

--- src/app/flight-crud/flight-crud.component.spec.ts
+++ src/app/flight-crud/flight-crud.component.spec.ts
@@ -1,21 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FlightCrudComponent } from './flight-crud.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { ApiService } from '../shared/api.service';
 
 describe('FlightCrudComponent', () => {
   let component: FlightCrudComponent;
   let fixture: ComponentFixture<FlightCrudComponent>;
+  let apiService: ApiService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [FlightCrudComponent],
+      providers: [
+        { provide: ApiService, useValue: jasmine.createSpyObj('ApiService', ['postFlight', 'getFlight', 'deleteFlight', 'updateFlight']) }
+      ]
+    })
+      .compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FlightCrudComponent]
-    });
     fixture = TestBed.createComponent(FlightCrudComponent);
     component = fixture.componentInstance;
+    apiService = TestBed.inject(ApiService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add flight', () => {
+    const flight = { polazak: 'Test origin', dolazak: 'Test destination', vremePoletanja: '12:00' };
+    component.formValue.setValue(flight);
+    spyOn(apiService, 'postFlight').and.returnValue(of({}));
+    spyOn(apiService, 'getFlight').and.returnValue(of([]));
+    component.postFlightDetails();
+    expect(apiService.postFlight).toHaveBeenCalledWith(flight);
+    expect(apiService.getFlight).toHaveBeenCalled();
+  });
+
+  it('should get all flights', () => {
+    const flights = [{ id: 1, polazak: 'Test origin', dolazak: 'Test destination', vremePoletanja: '12:00' }];
+    spyOn(apiService, 'getFlight').and.returnValue(of(flights));
+    component.getAllFlights();
+    expect(apiService.getFlight).toHaveBeenCalled();
+    expect(component.flightData).toEqual(flights);
+  });
+
 });
--- src/app/flights/flights.component.spec.ts
+++ src/app/flights/flights.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FlightsComponent } from './flights.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 describe('FlightsComponent', () => {
   let component: FlightsComponent;
@@ -8,14 +9,45 @@ describe('FlightsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [FlightsComponent]
     });
     fixture = TestBed.createComponent(FlightsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  it('form invalid when empty', () => {
+    expect(component.angForm.valid).toBeFalsy();
+  });
+
+  it('should not add a flight if form is invalid', () => {
+    const pocetnaDestinacijaInput = document.createElement('input');
+    pocetnaDestinacijaInput.value = '';
+    const finalnaDestinacijaInput = document.createElement('input');
+    finalnaDestinacijaInput.value = 'From';
+    const kompanijaInput = document.createElement('input');
+    kompanijaInput.value = 'To';
+    const datumInput = document.createElement('input');
+    datumInput.value = '';
+    const vremepInput = document.createElement('input');
+    vremepInput.value = '';
+    const vremedInput = document.createElement('input');
+    vremedInput.value = '';
+    component.angForm = new FormGroup({
+      pocetnaDestinacija: new FormControl("", Validators.compose([Validators.required, Validators.minLength(6)])),
+      finalnaDestinacija: new FormControl("", Validators.compose([Validators.required, Validators.minLength(6)])),
+      kompanija: new FormControl("", Validators.compose([Validators.required, Validators.minLength(6)])),
+      datum: new FormControl("", Validators.compose([Validators.required, Validators.minLength(6)])),
+      vremep: new FormControl("", Validators.compose([Validators.required, Validators.minLength(4)])),
+      vremed: new FormControl("", Validators.compose([Validators.required, Validators.minLength(4)])),
+    });
+    component.addFlights(pocetnaDestinacijaInput, finalnaDestinacijaInput, kompanijaInput, datumInput, vremepInput, vremedInput);
+  expect(component.flights.length).toBe(3);
+  });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
